e6fdc6fcd736b30aa566b0d4fc479d37f714dddb
thautwarm <twshere@outlook.com>
2020-08-12 01:49:48 +0900

    finish

--- README.md
+++ README.md
@@ -4,3 +4,100 @@
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://thautwarm.github.io/Sequent.jl/dev)
 [![Build Status](https://travis-ci.com/thautwarm/Sequent.jl.svg?branch=master)](https://travis-ci.com/thautwarm/Sequent.jl)
 [![Coverage](https://codecov.io/gh/thautwarm/Sequent.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/thautwarm/Sequent.jl)
+
+This package implements sequent calculus in Julia programming language, which gives you the capability of easily and formally describing semantics of your system.
+
+To running the whole tests, you should add an extra package
+```julia
+pkg> add https://github.com/thautwarm/HMRowUnification.jl
+```
+
+## Example: Natural Number
+
+```julia
+@data Nat begin
+    Z
+    S(Nat)
+end
+
+rule_z = @sequent FOLD_Z begin
+    _ ⊢ₛ Z => 0
+end
+
+rule_s = @sequent FOLD_S begin
+    _ ⊢ₛ term => n
+    _ ⊢ₛ S(term) => n + 1 
+end
+
+@semantics [rule_z, rule_s]
+
+(nothing ⊢ₛ Z) == 0
+(nothing ⊢ₛ S(S(Z))) == 2
+```
+
+## Example: Monomorphic Type Inference
+
+```julia
+@data ML begin
+    LApp(ML, ML)
+    LFun(Symbol, ML)
+    LVar(Symbol)
+    LVal(Int)
+end
+
+
+ImD = Base.ImmutableDict
+
+struct Sigma{O}
+    unbox :: ImD{Symbol, O}
+end
+
+Sigma{O}() where O = Sigma(ImD{Symbol, O}())
+
+Base.getindex(s::Sigma{O}, x::Pair{Symbol, A}) where {O, A<:O} =
+    Sigma(ImD(s.unbox, x))
+Base.getindex(s::Sigma{O}, x::Symbol) where O = 
+    s.unbox[x]
+
+(s::Sigma{O})(x::Pair{Symbol, A}) where {O, A<:O} = ImD(s, x)
+
+r_app = @sequent APP begin
+    (Γ, σ) ⊢ʷ a => ta
+    (Γ, σ) ⊢ʷ f => Arrow(ta′, tr)
+    true = Γ.unify(ta, ta′)
+    (Γ, σ) ⊢ʷ LApp(f, a) => tr
+end
+
+r_var = @sequent VAR begin
+    (Γ, σ) ⊢ʷ LVar(s) => σ[s]
+end
+
+r_fun = @sequent FUN begin
+    a′ = Γ.new_tvar()
+    (Γ, σ[a => a′]) ⊢ʷ r => tr
+    (Γ, σ) ⊢ʷ LFun(a, r) => Arrow(a′, tr)
+end
+
+r_val = @sequent VAL begin
+    _ ⊢ʷ LVal(_) => Nom(:int)
+end
+
+@semantics [r_app, r_fun, r_val, r_var]
+
+@testset "ML" begin
+    tctx = HMT[]
+    Γ = mk_tcstate(tctx)
+    σ = Sigma{HMT}()
+    int_t = Nom(:int)
+    σ = σ[:add => Arrow(int_t, Arrow(int_t, int_t))]
+    term = 
+        LFun(:a,
+            LApp(
+                LApp(
+                    LVar(:add),
+                    LVar(:a)),
+                LVar(:a)))
+    infer_ty =  (Γ, σ) ⊢ʷ term
+    @test Γ.prune(infer_ty) == Arrow(int_t, int_t)
+end
+```
--- src/Sequent.jl
+++ src/Sequent.jl
@@ -1,5 +1,134 @@
 module Sequent
+using MLStyle
+export @sequent, @semantics
+@nospecialize
+@data Judgement begin
+    Assertion(
+        line::LineNumberNode,
+        rule_name::Symbol,
+        vectorized::Bool,
+        judge_op::Symbol,
+        context,
+        pattern,
+        ret)
+    
+    EqMatch(line::LineNumberNode, pattern, value)
+end
 
-# Write your package code here.
+struct SequentDef
+    conditions :: Vector{Judgement}
+    case :: Assertion
+end
 
+macro sequent(name, formula)
+    name isa Symbol || error("formula name must be a symbol!")
+    @match formula Expr(:block, stmts...) =>
+    begin
+        conditions = Judgement[]
+        line = __source__
+        for stmt in stmts
+            @switch stmt begin
+            @case line::LineNumberNode
+                continue
+            @case :($a = $b)
+                push!(conditions, EqMatch(line, a, b))
+                continue
+            @case Expr(:call,
+                    :(=>) && let is_vec = false end ||
+                    :(.=>) && let is_vec = true end,
+                    Expr(:call, judge_op::Symbol, context, pattern),
+                    result) &&
+                    if startswith(string(judge_op), "⊢") end
+                push!(conditions,
+                      Assertion(
+                        line, name, is_vec,
+                        judge_op, context, pattern, result))
+                continue
+            end
+        end
+        length(conditions) >= 1 && conditions[end] isa Assertion ||
+        error("malformed sequent definition $name at $__source__")
+
+        case = pop!(conditions)
+        SequentDef(conditions, case)
+    end
+end
+
+function semantics(seqs::Vector{SequentDef}, __source__::LineNumberNode)
+    groups = Dict{Symbol, Vector{SequentDef}}()
+    for seq in seqs
+        group = get!(groups, seq.case.judge_op) do
+            SequentDef[]
+        end
+        push!(group, seq)
+    end
+    ctx_sym = gensym("ctx")
+    term_sym = gensym("term")
+    code = Expr(:block)
+    for (funcname, group) in groups
+        when_cases = Tuple{Expr, Any}[]
+        for seq in group
+            case_clauses = Expr(:block)
+            main_case = Expr(:tuple, seq.case.context, seq.case.pattern)
+            push!(
+                case_clauses.args,
+                :($main_case = ($ctx_sym, $term_sym)))
+            for cond in seq.conditions
+                @match cond begin
+                    EqMatch(line, pattern, value) =>
+                        begin
+                            value = Expr(:block, line, value)
+                            push!(case_clauses.args, :($pattern = $value))
+                        end
+                    Assertion(
+                        line, name, is_vec,
+                        judge_op, context, pattern, result) =>
+                        begin
+                            context === :_ && (context = :nothing)
+                            fcall = is_vec ?
+                                :(($term_sym -> $judge_op($context, $term_sym)).($pattern)) :
+                                :($judge_op($context, $pattern))
+                            fcall = Expr(:block, line, fcall)
+                            push!(case_clauses.args, :($result = $fcall))
+                        end
+                end
+            end
+            push!(
+                when_cases,
+                (case_clauses, seq.case.ret)
+            )
+        end
+        hd, tl = when_cases[1], @view when_cases[2:end]
+        when_block = Expr(:block)
+        push!(when_block.args, hd[2])
+        for (binding, result) in tl
+            push!(when_block.args, :(@when $binding))
+            push!(when_block.args, result)
+        end
+        push!(when_block.args, :(@otherwise))
+        push!(when_block.args, :($error("unrecognised input" * $string($term_sym))))
+        match_expression =
+            Expr(:let, 
+                hd[1],
+                when_block)
+        
+        push!(
+            code.args,
+            Expr(
+                :function,
+                Expr(:call, funcname, ctx_sym, term_sym),
+                Expr(:block,
+                    Expr(
+                        :macrocall,
+                        GlobalRef(MLStyle, Symbol("@when")),
+                        __source__,
+                        match_expression))))
+    end
+    code
+end
+
+macro semantics(seqs)
+    esc(semantics(__module__.eval(seqs), __source__))
+end
+@specialize
 end
